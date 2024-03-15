e67eac029167cbd39c474ab38baf36f7d740124d
Gabriel Viana <45436876+VianaCoder@users.noreply.github.com>
2024-03-15 03:06:21 +0000

    New message template

--- server/modules/telegramBot.js
+++ server/modules/telegramBot.js
@@ -3,14 +3,14 @@ require("dotenv").config();
 
 module.exports = {
 
-    async reportNewState (server, test) {
+    async reportNewState (server, test, state) {
 
         const TELEGRAM_TOKEN = process.env.TELEGRAM_TOKEN
         const CHAT_ID = process.env.CHAT_ID
     
         return new Promise ((resolve, reject) => {
 
-            message = `<b>NewState in ${test}}: ${server.lastState}</b> \n<b>Server:</b> ${server.name}\n<b>Hostname:</b> ${server.hostname}`
+            message = `🚨 <b> NewState in ${test}:</b> ${state} \n\n<b>Server:</b> ${server.name}\n\n<b>Hostname:</b> ${server.hostname}`
     
             axios.get(`https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage`, { params: { 'chat_id': CHAT_ID, 'text': message, 'parse_mode': 'HTML'}})
                 .then(function(response) {
@@ -29,7 +29,7 @@ module.exports = {
     
         return new Promise ((resolve, reject) => {
 
-            message = `<b>Report Problem in ${test}}\n <b>Description:</b> ${description}`
+            message = `<b>Report Problem in ${test}\n <b>Description:</b> ${description}`
     
             axios.get(`https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage`, { params: { 'chat_id': CHAT_ID, 'text': message, 'parse_mode': 'HTML'}})
                 .then(function(response) {
